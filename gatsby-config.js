516f56bcff2fe9cfa3f830103c6cfb155c72384c
Todd Evans <evansmtodd@gmail.com>
2022-06-09 13:50:16 +1000

    Added gatsby image plugins and put an image on the home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     title: `Gatsby Blog`,
     siteUrl: `https://www.awesome-gatsby-blog.netlify.app`,
   },
-  plugins: ["gatsby-plugin-netlify"],
+  plugins: [
+    "gatsby-plugin-netlify",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
