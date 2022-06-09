d583391c1141247fcf8ac18e4a70a42f59746ca9
Todd Evans <evansmtodd@gmail.com>
2022-06-09 12:57:21 +1000

    Updated siteUrl in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `awesome-gatsby-blog.netlify.app`,
   },
   plugins: ["gatsby-plugin-netlify"],
 }
