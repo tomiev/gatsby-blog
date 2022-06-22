72396080bd9f3a190e29d326cc5be54d707bb843
Todd Evans <evansmtodd@gmail.com>
2022-06-22 16:14:51 +1000

    Added dynamic hero image to blog post component

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "gatsby-plugin-netlify",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
